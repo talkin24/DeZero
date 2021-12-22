fcc2ab57ed24a735c101e6bbebf4dcfea4a403eb
talkin24 <cresc5talkin@gmail.com>
2021-12-23 01:35:37 +0900

    [ADD] Optimization

---
+++ steps/step28.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from sys import path
+
+
+if '__file__' in globals():  # __file__ 이라는 전역변수가 정의되어 있는지 확인
+    import os
+    import sys
+    # 현재 파일이 위치한 디렉토리의 부모 디렉토리를 모듈 검색 경로에 추가
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import numpy as np
+from dezero import Function
+from dezero import Variable
+import math
+from dezero.utils import plot_dot_graph
+
+
+def rosenbrock(x0, x1):
+    y = 100 * (x1 - x0 ** 2) ** 2 + (1 - x0) ** 2
+    return y
+
+
+x0 = Variable(np.array(0.0))
+x1 = Variable(np.array(2.0))
+lr = 0.001
+iters = 100000
+# 1000, 10000
+
+for i in range(iters):
+    print(x0, x1)
+
+    y = rosenbrock(x0, x1)
+
+    x0.cleargrad()
+    x1.cleargrad()
+    y.backward()
+
+    x0.data -= lr * x0.grad
+    x1.data -= lr * x1.grad
