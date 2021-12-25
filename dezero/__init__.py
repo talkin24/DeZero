82546a92135717d2dc6cd096e4a07ab4ff5d3578
talkin24 <cresc5talkin@gmail.com>
2021-12-25 23:51:25 +0900

    [ADD] 고차 미분 구현

--- dezero/__init__.py
+++ dezero/__init__.py
@@ -4,7 +4,7 @@
 is_simple_core = False
 
 if is_simple_core:
-    from dezero.core_simple import Variable, as_array
+    from dezero.core_simple import Variable
     from dezero.core_simple import Function
     from dezero.core_simple import using_config
     from dezero.core_simple import no_grad
@@ -13,6 +13,12 @@ if is_simple_core:
     from dezero.core_simple import setup_variable
 
 else:
-    pass
+    from dezero.core import Variable
+    from dezero.core import Function
+    from dezero.core import using_config
+    from dezero.core import no_grad
+    from dezero.core import as_array
+    from dezero.core import as_variable
+    from dezero.core import setup_variable
 
 setup_variable()
