74fc223a717895923f7fd49f40307e4d239d1c97
talkin24 <cresc5talkin@gmail.com>
2021-12-27 23:59:05 +0900

    [ADD] 고차 미분 자동화

---
+++ steps/step33.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from sys import path
+
+
+if '__file__' in globals():  # __file__ 이라는 전역변수가 정의되어 있는지 확인
+    import os
+    import sys
+    # 현재 파일이 위치한 디렉토리의 부모 디렉토리를 모듈 검색 경로에 추가
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import numpy as np
+from dezero import Function
+from dezero import Variable
+import math
+from dezero.utils import plot_dot_graph
+
+
+def f(x):
+    y = x ** 4 - 2 * x ** 2
+    return y
+
+
+x = Variable(np.array(2.0))
+iters = 10
+
+
+for i in range(iters):
+    print(i, x)
+
+    y = f(x)
+    x.cleargrad()
+    y.backward(create_graph=True)
+
+    gx = x.grad
+    x.cleargrad()
+    gx.backward()
+    gx2 = x.grad
+
+    x.data -= gx.data / gx2(x.data)
