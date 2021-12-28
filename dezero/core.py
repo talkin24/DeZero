a094df2e24bd4d855ad9c87dd7a3b4a223e8dff8
talkin24 <cresc5talkin@gmail.com>
2021-12-28 23:39:05 +0900

    [ADD] Sin, Cos function

--- dezero/core.py
+++ dezero/core.py
@@ -177,7 +177,7 @@ class Pow(Function):
         return y
 
     def backward(self, gy):
-        x = self.inputs[0]
+        x, = self.inputs
         c = self.c
         gx = c * x ** (c - 1) * gy
         return gx
