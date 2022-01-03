406f79620eb023167f0cd4efb06bf58edaa4838f
talkin24 <cresc5talkin@gmail.com>
2022-01-04 02:20:23 +0900

    [ADD] Liner Regression

---
+++ steps/step42.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from sys import path
+
+
+if '__file__' in globals():  # __file__ 이라는 전역변수가 정의되어 있는지 확인
+    import os
+    import sys
+    # 현재 파일이 위치한 디렉토리의 부모 디렉토리를 모듈 검색 경로에 추가
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import numpy as np
+from dezero import Variable
+import dezero.functions as F
+
+
+np.random.seed(0)
+x = np.random.rand(100, 1)
+y = 5 + 2 * x + np.random.rand(100, 1)
+x, y = Variable(x), Variable(y)  # 생략 가능
+
+W = Variable(np.zeros((1, 1)))
+b = Variable(np.zeros(1))
+
+
+def predict(x):
+    y = F.matmul(x, W) + b
+    return y
+
+
+def mean_squared_error(x0, x1):
+    diff = x0 - x1
+    return F.sum(diff ** 2) / len(diff)
+
+
+lr = 0.1
+iters = 100
+
+for i in range(iters):
+    y_pred = predict(x)
+    loss = mean_squared_error(y, y_pred)
+
+    W.cleargrad()
+    b.cleargrad()
+    loss.backward()
+
+    W.data -= lr * W.grad.data
+    b.data -= lr * b.grad.data
+    print(W, b, loss)
