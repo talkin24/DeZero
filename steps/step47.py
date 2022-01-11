2aa2be3fe046982ab5326f72787b38dfd1b021a5
talkin24 <cresc5talkin@gmail.com>
2022-01-12 01:12:27 +0900

    [ADD] softmax-simple

---
+++ steps/step47.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from sys import path
+
+
+if '__file__' in globals():  # __file__ 이라는 전역변수가 정의되어 있는지 확인
+    import os
+    import sys
+    # 현재 파일이 위치한 디렉토리의 부모 디렉토리를 모듈 검색 경로에 추가
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import numpy as np
+from dezero import Variable, as_variable
+import dezero.functions as F
+from dezero.models import MLP
+
+model = MLP((10, 3))
+
+x = np.array([[0.2, -0.4]])
+y = model(x)
+print(y)
+
+
+def softmax1d(x):
+    x = as_variable(x)
+    y = F.exp(x)
+    sum_y = F.sum(y)
+    return y / sum_y  # 브로드캐스트 발생
+
+
+x = Variable(np.array([[0.2, -0.4]]))
+y = model(x)
+p = softmax1d(y)
+print(y)
+print(p)
