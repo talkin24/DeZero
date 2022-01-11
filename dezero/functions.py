0da1ccb63b8cba5098a7ca8fe985100987d46ab9
talkin24 <cresc5talkin@gmail.com>
2022-01-12 01:16:23 +0900

    [UPDATE] softmax

--- dezero/functions.py
+++ dezero/functions.py
@@ -284,3 +284,33 @@ class GetItemGrad(Function):
 
     def backward(self, ggx):
         return get_item(ggx, self.slices)
+
+
+def softmax_simple(x, axis=1):
+    x = as_variable(x)
+    y = exp(x)
+    sum_y = sum(y, axis=axis, keepdims=True)
+    return y / sum_y
+
+
+class Softmax(Function):
+    def __init__(self, axis=1):
+        self.axis = axis
+
+    def forward(self, x):
+        xp = cuda.get_array_module(x)
+        y = x - x.max(axis=self.axis, keepdims=True)
+        y = xp.exp(y)
+        y /= y.sum(aixs=self.axis, keepdims=True)
+        return y
+
+    def backward(self, gy):
+        y = self.outputs[0]()
+        gx = y * gy
+        sumdx = gx.sum(axis=self.axis, keepdims=True)
+        gx -= y * sumdx
+        return gx
+
+
+def softmax(x, axis=1):
+    return Softmax(axis)(x)
