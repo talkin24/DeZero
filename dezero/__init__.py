4d0d919fb7c63cd3c2eb641e1e37a7c03c410c84
talkin24 <cresc5talkin@gmail.com>
2022-01-19 23:37:30 +0900

    [ADD] Dataloader

--- dezero/__init__.py
+++ dezero/__init__.py
@@ -23,6 +23,7 @@ else:
     from dezero.core import setup_variable
     from dezero.layers import Layer
     from dezero.models import Model
+    from dezero.dataloaders import DataLoader
 
     import dezero.datasets
 
