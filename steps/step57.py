e899d1babaf93493ec3fae2b98d4d186887ef343
talkin24 <cresc5talkin@gmail.com>
2022-01-31 16:52:49 +0900

    [UPDATE] dataloader

--- steps/step57.py
+++ steps/step57.py
@@ -3,6 +3,7 @@ from sys import path
 import os
 import sys
 
+
 if '__file__' in globals():  # __file__ 이라는 전역변수가 정의되어 있는지 확인
 
     # 현재 파일이 위치한 디렉토리의 부모 디렉토리를 모듈 검색 경로에 추가
@@ -14,4 +15,12 @@ import dezero.functions as F
 
 
 x1 = np.random.rand(1, 3, 7, 7)  # batch size = 1
-col1 = F.im2col
+col1 = F.im2col(x1, kernel_size=5, stride=1, pad=0, to_matrix=True)
+print(col1.shape)
+
+x2 = np.random.rand(10, 3, 7, 7)  # batch size = 10
+kernel_size = (5, 5)
+stride = (1, 1)
+pad = (0, 0)
+col2 = F.im2col(x2, kernel_size, stride, pad, to_matrix=True)
+print(col2.shape)
